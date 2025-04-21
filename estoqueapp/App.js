99e9a7953ab05cf5a1f0054f5184687deba235bb
Caio Richard <richarddeabreu2017@gmail.com>
2025-04-21 11:39:57 -0300

    feat: criação do ambiente de desenvolvimento com JS como base

---
+++ estoqueapp/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Aqui é onde será criado o frontend do aplicativo!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
