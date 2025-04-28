09ac3a1e49e3e8b313c86d883c31a5ede7d6dc47
Caio Richard <richarddeabreu2017@gmail.com>
2025-04-28 17:12:51 -0300

    Feat: Base do rascunho  da interface do app

--- estoqueapp/App.js
+++ estoqueapp/App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
+import styles from './styles/styles';
+
+// Dados simulados
+const produtos = [
+  { id: '1', nome: 'Cadeira de Escritório' },
+  { id: '2', nome: 'Mesa de Madeira' },
+  { id: '3', nome: 'Armário de Aço' },
+  { id: '4', nome: 'Estante Pequena' },
+  { id: '5', nome: 'Sofá 2 Lugares' },
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Aqui é onde será criado o frontend do aplicativo!</Text>
+      <Text style={styles.title}>Produtos em estoque</Text>
+
+      <FlatList
+        data={produtos}
+        keyExtractor={(item) => item.id}
+        style={styles.lista}
+        renderItem={({ item }) => (
+          <View style={styles.linha}>
+            <Text style={styles.produto}>{item.nome}</Text>
+          </View>
+        )}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ estoqueapp/styles/styles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'pink',
+      paddingTop: 60,
+      paddingHorizontal: 20,
+    },
+    title: {
+      fontSize: 26,
+      fontWeight: 'bold',
+      marginBottom: 20,
+      borderRadius:8,
+      textAlign: 'center',
+      backgroundColor: 'white',
+      color: 'pink',
+    },
+    lista: {
+      marginTop: 10,
+    },
+    linha: {
+      padding: 15,
+      marginBottom: 10,
+      backgroundColor: '#f2f2f2',
+      borderRadius: 8,
+    },
+    produto: {
+      fontSize: 18,
+      color: '#333',
+    },
+  });
+
+  export default styles;
